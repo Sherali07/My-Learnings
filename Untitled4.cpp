cfc3caca6fb08f9c4e09d2dde1c99522a38ffaba
Sherali07 <127723080+Sherali07@users.noreply.github.com>
2023-03-13 12:30:55 +0500

    Add files via upload

---
+++ Untitled4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[10],i,sum=0;
+	cout<<"Enter 10 array element: ";
+	for(i=0;i<10;i++)
+	cin>>arr[i];
+	for(i=0;i<10;i++)
+	sum= sum+arr[i];
+	cout<<"\nsum of all array elements="<<sum;
+	cout<<endl;
+	return 0;
+}
